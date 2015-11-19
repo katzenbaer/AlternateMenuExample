72da56cf34a6f72a25a7c485f5b500416dbdd014
Terrence Katzenbaer <tkatzenbaer@apple.com>
2015-11-19 10:49:55 -0800

    Initial Commit

---
+++ AlternateMenuExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AlternateMenuExample
+//
+//  Created by Terrence Katzenbaer on 11/19/15.
+//  Copyright © 2015 Terrence Katzenbaer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AlternateMenuExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AlternateMenuExample
+//
+//  Created by Terrence Katzenbaer on 11/19/15.
+//  Copyright © 2015 Terrence Katzenbaer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
